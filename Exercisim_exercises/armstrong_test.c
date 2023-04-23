36d470748d9207d2376adcd9eb243daf968ddbee
Bigizic <olalekanisaac75@gmail.com>
2023-04-23 14:18:47 -0700

    armstrong_number

---
+++ Exercisim_exercises/armstrong_test.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main()
+{
+    is_armstrong_number(153);
+    is_armstrong_number(370);
+    is_armstrong_number(9474);
+    is_armstrong_number(1634);
+    is_armstrong_number(8208);
+    is_armstrong_number(123);
+
+    return 0;
+}
