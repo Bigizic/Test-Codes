f2deff91572b00f2bb394069e6a15eed233e9250
Bigizic <olalekanisaac75@gmail.com>
2023-04-29 14:06:35 -0700

    isogram

--- Exercisim_exercises/isogram.c
+++ Exercisim_exercises/isogram.c
@@ -62,3 +62,60 @@ bool is_isogram(const char phrase[])
     }
     return (true);
 }
+
+
+/**
+* isogram_prompt - function that prints a prompt and read user prompt
+*
+* Description: this function prints a prompt message
+* it takes an input with limit of 1024 characters
+* then gets the length of the input and minus one
+* from the last char to check if the input has
+* a new-line char, normally the input always ends with
+* new-line. It minus one to have access to the last
+* char and tells the compiler to remove the new-line
+* and update it to NULL terminated which indicates
+* end of the string.
+*
+* Return: 0 if success
+*/
+bool isogram_prompt(void)
+{
+	const char user_input[1024];
+	size_t len;
+	bool status;
+
+	printf("Enter a word or phrase: ");
+	fgets(user_input, sizeof(user_input), stdin);
+
+	len = strlen(user_input); /* gets length of input */
+	if (len > 0 && user_input[len -1] == '\n')
+	{
+		user_input[len - 1] = '\0';
+	}
+	status = is_isogram(user_input);
+	result(user_input);
+	return (status);
+}
+
+
+/**
+* result - result msg
+*
+* @in: input
+*
+* Return: void
+*/
+int result(const char in[])
+{
+	bool x = isogram_prompt();
+
+	if (x)
+	{
+		printf("The input: %s, is an isogram.\n", in);
+	}
+	else
+	{
+		printf("The input: %s, is not an isogram.\n", in);
+	}
+}
