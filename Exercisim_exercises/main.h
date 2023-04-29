51cd47064575409937f89fd242f53af6e772b991
Bigizic <olalekanisaac75@gmail.com>
2023-04-29 14:06:23 -0700

    header

--- Exercisim_exercises/main.h
+++ Exercisim_exercises/main.h
@@ -13,8 +13,13 @@
 /* function prototypes */
 bool is_armstrong_number(int candidate);
 double square_root(double num);
+
+/* isogram */
 bool is_isogram(const char phrase[]);
 int _tolower(int c);
+int result(const char in[]);
+bool isogram_prompt(void);
+
 int compute(const char *lhs, const char *rhs);
 
 #endif
