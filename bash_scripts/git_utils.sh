fee92fda6c059d93e81ca1208f6f0b4b45e3ff55
Bigizic <olalekanisaac75@gmail.com>
2023-05-05 15:08:44 +0000

    update

--- bash_scripts/git_utils.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -n "Enter file name: "; read name
-echo -n "Enter commit message: "; read commit
-git add $name
-git commit -m $commit
-git push
--- bash_scripts/set_origin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -n "Enter access token: "; read access_token
-echo -n "Enter username: "; read username
-echo -n "Enter repository: "; read repo
-
-git remote set-url origin https://$access_token@github.com/$username/$repo.git
