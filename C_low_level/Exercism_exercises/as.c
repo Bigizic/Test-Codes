5a387157f3c7ab819db9692c388c8d914e8f2a69
Bigizic <olalekanisaac75@gmail.com>
2023-05-07 06:29:44 +0000

    as

--- C_low_level/Exercism_exercises/as.c
+++ C_low_level/Exercism_exercises/as.c
@@ -9,42 +9,53 @@
 
 char *phone_number_clean(const char *input)
 {
-    int i, c, n;
-    long unsigned int result = 0;
-    unsigned int len = strlen(input), str_len;
-    int (*store_digit)(int) = &isdigit;
+	int i, c, n;
+	long unsigned int result = 0;
+	unsigned int len = strlen(input), str_len, j;
+	int (*store_digit)(int) = &isdigit;
 	char str[BUFFER_SIZE];
 	char *zeros = "0";
 
-    if (input == NULL)
-        return (NULL);
-    if (len == 0)
-        return (NULL);
+	if (input == NULL)
+		return (NULL);
+	if (len == 0)
+		return (NULL);
 
-    if (len > 7)
-    {
-        for (i = 0; input[i] != '\0'; i++)
+	for (i = 0; input[i] != '\0'; i++)
         {
-	c = (*store_digit)(input[i]);
-            if (c)
-            {
-                result = result * 10 + (input[i] - '0');
-            }
-        }
+		c = (*store_digit)(input[i]);
+		if (c)
+		{
+			result = result * 10 + (input[i] - '0');
+		}
+	}
 	sprintf(str, "%d", c);
 	str_len = strlen(str);
 	if (str_len > 0 && str[str_len - 1] == '\n')
 		str[str_len - 1] = '\0';
-	if (str_len < 10)
+
+	if (str_len < 10 || (str_len > 10 && str[0] != 1))
 	{
 		for (n = 0; n < 10; n++)
 		{
 			printf("%s", zeros);
 		}
 	}
-    }
+
+	if (str[0] == 1 && str_len >= 10)
+	{
+		for (j = 0; j < str_len - 1; j++)
+		{
+			strcpy(str[j], str[j + 1]);
+		}
+		str_len--;
+		for (j = 0; j < str_len; j++)
+		{
+			printf("%s", str[j]);
+		}
+	}
 	printf("\n");
-    printf("%lu\n", result);
+	printf("%lu\n", result);
 	return (0);
 }
 
@@ -54,7 +65,7 @@ char *phone_number_clean(const char *input)
 */
 int main(void)
 {
-	const char *x = "123456789";
+	const char *x = "12234567890";
 	phone_number_clean(x);
 	
 	return (0);
