c65650606e37d88b80470777980bf1c1ffa3440d
Bigizic <olalekanisaac75@gmail.com>
2023-05-21 14:58:36 +0000

    Add hours.c

--- C_low_level/Exercism_exercises/hours.c
+++ C_low_level/Exercism_exercises/hours.c
@@ -4,20 +4,23 @@
 
 int main(int ac, char *av[])
 {
-	unsigned long int y = atoi(av[1]);
-	unsigned long int x;
-	unsigned long int i = 0;
+	int y = atoi(av[1]);
+	int x;
+	int i = 0;
 
 	(void)ac;
 
+	if (y < 0)
+		y += 24;
 	x = y % 24;
+
 	if (x < 10 && x != 0)
-		printf("The hour is 0%lu\n", x);
+		printf("The hour is 0%d\n", x);
 
 	else if (x > 9 && x != 0)
-		printf("The hour is %lu\n", x);
+		printf("The hour is %d\n", x);
 	else
-		printf("The hour is 0%lu\n", i);
+		printf("The hour is 0%d\n", i);
 
 	return (0);
 }
