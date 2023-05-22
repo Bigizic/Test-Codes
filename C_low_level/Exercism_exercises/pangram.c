61e2368618911f4ef1529fc14a0a07dd6eea9e3f
Bigizic <olalekanisaac75@gmail.com>
2023-05-22 02:14:41 +0000

    Add pangram.c

--- C_low_level/Exercism_exercises/pangram.c
+++ C_low_level/Exercism_exercises/pangram.c
@@ -15,7 +15,7 @@ int _convert_to_lower(int c)
 
 	else if (c >= 'a' && c <= 'z')
 	{
-		return (c + ('a' - 'a'))
+		return (c + ('a' - 'a'));
 	}
 	return (c);
 }
@@ -47,4 +47,44 @@ bool is_pangram(const char* sentence)
 		}
 	}
 	return sentence_cp == ((1 << 26) - 1);
+}
+
+/**
+* main - main code for is_pangram, takes input and compares it
+*
+* Return: void
+*/
+int main(void)
+{
+        char *phrase, phrase_buffer[1024];
+
+        printf("NOTE: A pangram is a sentence using every letter\nof the alphabet at least once. It doesn't matter\nif a letter is lower-case or upper-case.\nThe best known English pangram is:\nThe quick brown fox jumps over the lazy dog.\nNow enter a sentence! \n\n");
+
+        phrase = fgets(phrase_buffer, sizeof(phrase_buffer), stdin);
+        if (phrase == NULL)
+        {
+                printf("Error: Enter a word or sentence\n");
+                return (-1);
+        }
+        if (strlen(phrase_buffer) == 0)
+        {
+                printf("Error: Enter a word or sentence\n");
+                return (-1);
+        }
+
+        if (strlen(phrase_buffer) > 0 &&
+                        phrase_buffer[strlen(phrase_buffer) - 1] == '\n')
+                phrase_buffer[strlen(phrase_buffer) - 1]  = '\0';
+
+        if (is_pangram(phrase_buffer))
+        {
+                printf("Your input: {%s}, is a Pangram\n", phrase_buffer);
+                return (0);
+        }
+        else
+        {
+                printf("Your input: {%s}, is not a Pangram\n", phrase_buffer);
+                return (0);
+        }
+        return (0);
 }
