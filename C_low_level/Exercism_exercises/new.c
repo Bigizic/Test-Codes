a6caae2c38ed20db19b452d3dfb4f42d53575a37
Bigizic <olalekanisaac75@gmail.com>
2023-05-22 02:45:07 +0000

    Add new.c

---
+++ C_low_level/Exercism_exercises/new.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <math.h>
+
+/**
+* sum_of_squares - a function that tells the sum of the squares of
+* the first N natural numbers
+*
+* @number: unsigned int type also the N number to work on
+*
+* Return: sum of squares of number
+*/
+
+unsigned int sum_of_squares(unsigned int number)
+{
+    int count = 0, y, x, add = 0;
+
+    while (number > 0)
+    {
+        x = number - 1;
+        number = x;
+        count++;
+        if (count > 0)
+        {
+            y = pow(count, 2);
+        }
+	add += y;
+    }
+    printf("%d\n", add);
+    return (0);
+}
+
+int main(void)
+{
+	sum_of_squares();
+	return (0);
+}
