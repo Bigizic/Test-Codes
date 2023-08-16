3bcf66e305b1285f4c3044c19bc7c93bee5438fd
Bigizic <olalekanisaac75@gmail.com>
2023-08-16 12:29:50 +0000

    Add multiples.py

---
+++ python3/multiples.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def multiples():
+    new_array = []
+    sec_array = []
+    for x in range(21, 819, 21):
+        new_array.append(x)
+    for y in range(20, 595, 25):
+        sec_array.append(y)
+    print(new_array)
+    print(sec_array)
+
+multiples()
