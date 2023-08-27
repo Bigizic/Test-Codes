24a6e244935ac08e31a0e5898a14b0fbbe2f9e71
Bigizic <olalekanisaac75@gmail.com>
2023-08-27 19:56:10 +0000

    Update electricvehicle_db.sql

--- python3/data_from_data_dot_gov/electric_vehicles/1-electricvehicle_db.sql
+++ python3/data_from_data_dot_gov/electric_vehicles/1-electricvehicle_db.sql
@@ -1,1 +1,4 @@
 CREATE DATABASE IF NOT EXISTS ElectricVehicle;
+CREATE USER IF NOT EXISTS 'user_1738'@'localhost' IDENTIFIED BY 'groot';
+GRANT ALL PRIVILEGES ON ElectricVehicle . * TO 'user_1738'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'user_1738'@'localhost';
