bfc8fc29f2c2f79fec415b89c7418e6b95532421
Bigizic <olalekanisaac75@gmail.com>
2023-11-17 16:10:19 +0000

    Create unistall_mysql.sh

---
+++ web_utils/package_one/unistall_mysql.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+sudo systemctl stop mysql
+sudo apt purge mysql-server
+sudo apt purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*
+dpkg -l | grep mysql
+sudo rm -rf /var/lib/mysql
+sudo rm /etc/apt/sources.list.d/mysql.list
+sudo apt update
