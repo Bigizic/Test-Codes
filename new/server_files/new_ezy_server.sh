36b58d4f2ef0c12b45407e9b4a71574b92c0f1bc
Bigizic <olalekanisaac75@gmail.com>
2024-01-20 16:14:26 +0000

    Update new

---
+++ new/server_files/new_ezy_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# bash script for new server, proceeds to install certain libraries and packages
+sudo apt-get update
+sudo apt-get install python3-lxml -y
+sudo apt install gunicorn -y
+sudo apt-get install libmariadbclient-dev -y
+sudo apt-get install libmysqlclient-dev -y
+sudo apt install python3-pip -y
+sudo apt-get install -y libffi-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y python3.4-dev
+sudo apt-get install -y libpython3-dev
+sudo apt install mysql-server
+sudo apt-get update
+sudo apt-get upgrade
