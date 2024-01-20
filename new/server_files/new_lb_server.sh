6f6e37f546847704e1ff4d5512c0d20588fea96f
Bigizic <olalekanisaac75@gmail.com>
2024-01-20 17:09:17 +0000

    Update new_lb_server.sh

--- new/server_files/new_lb_server.sh
+++ new/server_files/new_lb_server.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt install -y net-tools
 sudo apt install -y  haproxy
+sudo apt install -y nginx
 sudo apt install -y certbot
 sudo apt-get -y install software-properties-common
 sudo systemctl restart haproxy
